ae1f1c01af634260de1ee496ffd44ab0cc792b58
Júlia Fernandes <juliamitc@gmail.com>
2024-02-18 17:24:35 -0300

    switch asp net core web to webapi

---
+++ Backend/GestaoPortifolio.API/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoPortifolio.API.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return await Task.FromResult(Ok("Olá mundo"));
+        }
+    }
+}
